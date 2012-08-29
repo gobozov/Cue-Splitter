a0c98ec84beb6f53cbdcfc9aa22b23773b4fe024
gobozov <gobozov@gmail.com>
2012-08-29 19:06:04 +0400

    flac

--- src/com/cue/splitter/util/FlacSplitter.java
+++ src/com/cue/splitter/util/FlacSplitter.java
@@ -8,10 +8,12 @@ import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
 import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
 import org.jaudiotagger.audio.flac.FlacFileReader;
 import org.jaudiotagger.audio.generic.GenericAudioHeader;
+import org.jaudiotagger.audio.mp3.MP3AudioHeader;
 import org.jaudiotagger.tag.TagException;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 
 /**
  * Created with IntelliJ IDEA.
@@ -26,26 +28,36 @@ public class FlacSplitter {
     public void splitCue(CueFile cueFile, String targetDir){
         String path = lookupTargetFile(cueFile);
         FlacFileReader flacReader = new FlacFileReader();
-        try {
-            AudioFile flacFile = flacReader.read(new File(path));
-            AudioHeader flacHeader = flacFile.getAudioHeader();
-            long bitRate = flacHeader.getBitRateAsNumber();
-
-
-        } catch (CannotReadException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (TagException e) {
-            e.printStackTrace();
-        } catch (ReadOnlyFileException e) {
-            e.printStackTrace();
-        } catch (InvalidAudioFrameException e) {
-            e.printStackTrace();
-        }
-
 
     }
+
+    private byte[] readChunk(File sourceFile, AudioHeader header, int startTime, int endTime) {
+        byte[] result = null;
+        RandomAccessFile randomAccessFile = null;
+        //long startIndex = header.getMp3StartByte();
+        long startIndex = 0;
+        int trackLengthMs = header.getTrackLength() * 1000;
+        long bitRate = header.getBitRateAsNumber();
+        long beginIndex = bitRate * 1024 / 8 / 1000 * startTime + startIndex;
+        long endIndex = beginIndex + bitRate * 1024 / 8 / 1000 * (endTime - startTime);
+        if (endTime > trackLengthMs)
+            endIndex = sourceFile.length() - 1;
+
+
+        System.out.println("mp3StartIndex = " + startIndex + " trackLengthMs = " + trackLengthMs + " bitRate = " + bitRate);
+        System.out.println("beginIndex = " + beginIndex + " endIndex = " + endIndex);
+        try {
+            randomAccessFile = new RandomAccessFile(sourceFile, "r");
+            randomAccessFile.seek(beginIndex);
+            int size = (int) (endIndex - beginIndex);
+            result = new byte[size];
+            randomAccessFile.read(result);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
 
 
     private String lookupTargetFile(CueFile cueFile) {
